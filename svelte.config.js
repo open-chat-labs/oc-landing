f341135840fab92db1e73f3714941e98711c4bab
Julian Jelfs <julian.jelfs@gmail.com>
2022-07-27 11:39:22 +0100

    baseline

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+// svelte.config.js
+const sveltePreprocess = require("svelte-preprocess");
+const path = require("path");
+
+const mixins = path.join(__dirname, "src", "styles", "mixins.scss");
+
+// this file is only used by the svelte language server so don't worry about it too much
+
+const preprocessOptions = {
+  sourceMaps: true,
+  defaults: {
+    script: "typescript",
+    style: "scss",
+  },
+  scss: {
+    // prependData: `@import 'v2/frontend/src/styles/mixins.scss';`,
+    prependData: `@use 'sass:math'; @import '${mixins}';`,
+  },
+};
+
+module.exports = {
+  preprocess: sveltePreprocess(preprocessOptions),
+};
