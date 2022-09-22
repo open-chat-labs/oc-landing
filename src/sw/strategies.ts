b20420749b0d488c442583810808a099094f63c8
Julian Jelfs <julian.jelfs@gmail.com>
2022-09-22 12:53:49 +0100

    use workbox for caching

---
+++ src/sw/strategies.ts
@@ -0,0 +1,86 @@
+import { AuthClient, IdbStorage } from "@dfinity/auth-client";
+import {
+    CacheFirst,
+    NetworkFirst,
+    StaleWhileRevalidate,
+    Strategy,
+    StrategyHandler,
+} from "workbox-strategies";
+import { handleRequest } from "./http_request";
+
+export async function isAnonymous() {
+    const client = await AuthClient.create({
+        idleOptions: { disableIdle: true },
+        storage: new IdbStorage(),
+    });
+    const id = client.getIdentity();
+    const principal = id.getPrincipal();
+    return principal.isAnonymous();
+}
+
+function toRequest(input) {
+    return typeof input === "string" ? new Request(input) : input;
+}
+
+export class IcHandler extends StrategyHandler {
+    async fetch(input: RequestInfo): Promise<Response> {
+        console.log(
+            "SW: cache miss (or revalidation), falling back to default ic service worker ",
+            input
+        );
+        return handleRequest(toRequest(input));
+    }
+}
+
+function createIcHandler(strategy: Strategy, handler: StrategyHandler): IcHandler {
+    return new IcHandler(strategy, {
+        event: handler.event,
+        request: handler.request,
+        params: handler.params,
+    });
+}
+
+export class StaleWhileRevalidateIfSignedIn extends StaleWhileRevalidate {
+    async _handle(request: Request, handler: StrategyHandler) {
+        if (await isAnonymous()) {
+            console.log(
+                "SW: StaleWhileRevalidate - not signed in, use network handler if not in cache ",
+                request.url
+            );
+            return handler.fetch(request);
+        }
+        console.log(
+            "SW: StaleWhileRevalidate signed in: use ic handler if not in cache",
+            request.url
+        );
+        return super._handle(request, createIcHandler(this, handler));
+    }
+}
+
+export class CacheFirstIfSignedIn extends CacheFirst {
+    async _handle(request: Request, handler: StrategyHandler) {
+        if (await isAnonymous()) {
+            console.log(
+                "SW: CacheFirst - not signed in, use network handler if not in cache ",
+                request.url
+            );
+            return handler.fetch(request);
+        }
+        console.log("SW: CacheFirst signed in: use ic handler if not in cache", request.url);
+        return super._handle(request, createIcHandler(this, handler));
+    }
+}
+
+export class NetworkFirstIfSignedIn extends NetworkFirst {
+    async _handle(request: Request, handler: StrategyHandler) {
+        if (await isAnonymous()) {
+            console.log(
+                "SW: NetworkFirst - not signed, use network handler if not in cache ",
+                request.url
+            );
+            return handler.fetch(request);
+        }
+        console.log("SW: NetworkFirst signed in: use ic handler if not in cache", request.url);
+        return super._handle(request, createIcHandler(this, handler));
+    }
+}
--- src/sw/sw.ts
+++ src/sw/sw.ts
@@ -1,16 +1,38 @@
-import { AuthClient, IdbStorage } from "@dfinity/auth-client";
 import { handleRequest } from "./http_request";
 
 declare const self: ServiceWorkerGlobalScope;
 
 const DEBUG = true;
 
-function createAuthClient() {
-    return AuthClient.create({
-        idleOptions: { disableIdle: true },
-        storage: new IdbStorage(),
-    });
-}
+//workbox config
+import { registerRoute, setDefaultHandler } from "workbox-routing";
+import {
+    CacheFirstIfSignedIn,
+    isAnonymous,
+    NetworkFirstIfSignedIn,
+    StaleWhileRevalidateIfSignedIn,
+} from "./strategies";
+
+const assetRegexes = [/main-.*[css|js]$/gi];
+const unversionedAssets = [/assets\/.*\.svg$/gi];
+
+// TODO make use of expiry and cacheable response plugins
+
+registerRoute((route) => {
+    return assetRegexes.some((re) => re.test(route.url.pathname));
+}, new CacheFirstIfSignedIn());
+
+registerRoute((route) => {
+    return unversionedAssets.some((re) => re.test(route.url.pathname));
+}, new StaleWhileRevalidateIfSignedIn());
+
+registerRoute((route) => {
+    return route.request.destination === "document";
+}, new NetworkFirstIfSignedIn());
+
+setDefaultHandler(({ request }) => {
+    return defaultHandler(request);
+});
 
 // Always install updated SW immediately
 self.addEventListener("install", () => {
@@ -25,96 +47,17 @@ self.addEventListener("activate", async () => {
     clients.forEach((client) => client.navigate(client.url));
 });
 
-const cacheName = "openchat_asset_cache";
-const assetRegexes = [/main-.*[css|js]$/gi, /spinner.svg$/gi];
-
-type CacheStrategy = "cache_first" | "network_first" | "no_cache";
-
-/**
- * We need to be pretty conservative here to avoid
- * a) leaving ourselves with stale cached resources
- * b) consuming megabytes of storage with low cache hit ratios
- *
- * Yes that makes offline usage harder but that is a secondary goal behind performance improvements
- */
-function getCacheStrategy(request: Request): CacheStrategy {
-    if (assetRegexes.some((re) => re.test(request.url))) {
-        return "cache_first";
-    } else if (["document"].includes(request.destination)) {
-        return "network_first";
-    }
-    return "no_cache";
-}
-
-function cacheFirstNetworkFallback(request: Request): Promise<Response> {
-    console.log("SW: checking SW cache for resource: ", request.url);
-    return caches.open(cacheName).then((cache) => {
-        // Go to the cache first
-        return cache.match(request.url).then((cachedResponse) => {
-            // Return a cached response if we have one
-            if (cachedResponse) {
-                console.log("SW: cache hit for: ", request.url);
-                return cachedResponse;
-            }
-
-            console.log("SW: cache miss for: ", request.url);
-            return handleRequest(request).then((resp) => {
-                cache.put(request, resp.clone());
-                return resp;
-            });
-        });
-    });
-}
-
-function networkFirstCacheFallback(request: Request): Promise<Response> {
-    return caches.open(cacheName).then((cache) => {
-        function serveFromCache() {
-            console.log(
-                "SW: resource failed to load from the network, falling back to cache: ",
-                request.url
-            );
-            // If the network is unavailable, get
-            return cache.match(request.url);
-        }
-        // Go to the network first
-        console.log("SW: attempting to request resource from the network: ", request.url);
-        return handleRequest(request)
-            .then((resp) => {
-                if (resp.ok) {
-                    console.log("SW: resource loaded from the network: ", request.url);
-                    cache.put(request, resp.clone());
-                    return resp;
-                } else {
-                    return serveFromCache();
-                }
-            })
-            .catch(serveFromCache);
-    });
-}
-
-function checkCache(request: Request): Promise<Response> {
-    const strategy = getCacheStrategy(request);
-    // Check if this is a request for an image
-    if (strategy === "cache_first") {
-        return cacheFirstNetworkFallback(request);
-    } else if (strategy === "network_first") {
-        return networkFirstCacheFallback(request);
-    } else {
-        console.log("SW: bypassing sw cache for resource: ", request.url);
-        return handleRequest(request);
-    }
-}
-
-async function checkAuthStatus(request: Request): Promise<Response> {
-    const client = await createAuthClient();
-    const id = client.getIdentity();
-    const principal = id.getPrincipal();
-    const anon = principal.isAnonymous();
-    if (anon) {
+async function defaultHandler(request: Request): Promise<Response> {
+    if (await isAnonymous()) {
+        console.log("SW: default handler - not signed in falling back to network: ", request.url);
         return fetch(request);
     } else {
         try {
-            return checkCache(request);
+            console.log(
+                "SW: default handler - signed in falling back to default ic service worker ",
+                request.url
+            );
+            return handleRequest(request);
         } catch (e) {
             const error_message = String(e);
             console.error(error_message);
@@ -127,8 +70,3 @@ async function checkAuthStatus(request: Request): Promise<Response> {
         }
     }
 }
-
-// Intercept and proxy all fetch requests made by the browser or DOM on this scope.
-self.addEventListener("fetch", (event: FetchEvent) => {
-    event.respondWith(checkAuthStatus(event.request));
-});
